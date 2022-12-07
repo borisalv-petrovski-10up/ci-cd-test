9773200aaa6749a8e2afb7e84f1c57faae850451
Borisalv <borislav.petrovski@get10up.com>
2022-12-07 11:01:47 +0200

    fix linter errors

--- cmd/flexible/main.go
+++ cmd/flexible/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/borisalv-petrovski-10up/ci-cd-test/services/app-ae-flexible/handlers"
 )
 
 func main() {
 	http.HandleFunc("/a", handlers.UniqueNames)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
 		panic(err)
 	}
 }
