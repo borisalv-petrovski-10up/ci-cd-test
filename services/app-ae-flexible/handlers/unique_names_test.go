93d85e58c653baa82c3839520d513fdae4265b8f
Borisalv <borislav.petrovski@get10up.com>
2022-12-19 11:21:25 +0200

    add tests

--- services/app-ae-flexible/handlers/unique_names_test.go
+++ services/app-ae-flexible/handlers/unique_names_test.go
@@ -1,6 +1,16 @@
 package handlers_test
 
-/*func TestUniqueNames(t *testing.T) {
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/borisalv-petrovski-10up/ci-cd-test/services/app-ae-flexible/handlers"
+)
+
+func TestUniqueNames(t *testing.T) {
 	// Arrange
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	res := httptest.NewRecorder()
@@ -11,4 +21,3 @@ package handlers_test
 	// Assert
 	assert.Equal(t, "Hello, [Samuel John Falcoooooooooooooo]!", res.Body.String())
 }
-*/
