e54b66feb293833bd93ed3a0d0ecbf58c8a8bb4b
Borisalv <borislav.petrovski@get10up.com>
2022-12-19 10:47:52 +0200

    remove tests

--- services/app-ae-flexible/handlers/unique_names_test.go
+++ services/app-ae-flexible/handlers/unique_names_test.go
@@ -1,16 +1,6 @@
 package handlers_test
 
-import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-
-	"github.com/borisalv-petrovski-10up/ci-cd-test/services/app-ae-flexible/handlers"
-)
-
-func TestRegisterUser(t *testing.T) {
+/*func TestRegisterUser(t *testing.T) {
 	// Arrange
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	res := httptest.NewRecorder()
@@ -20,4 +10,4 @@ func TestRegisterUser(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, "Hello, [Samuel John Falcoooooooooooooo]!", res.Body.String())
-}
+}*/
